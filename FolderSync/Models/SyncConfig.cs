30983b7ecadd528acbe8b27498985eb9542d794d
WillSchmidt97 <willschmidt1997@gmail.com>
2025-11-20 13:52:51 -0300

    feat: initial project scaffold

---
+++ FolderSync/Models/SyncConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync.Models
+{
+    public class SyncConfig
+    {
+        public string SourcePath { get; set; }
+        public string ReplicaPath { get; set; }
+        public int IntervalSeconds { get; set; }
+        public string LogFilePath { get; set; }
+    }
+}
--- FolderSync/Program.cs
+++ FolderSync/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FolderSync
+{
+    internal class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
